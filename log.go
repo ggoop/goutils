e1a40ba981dfefaf5f077fe750c6d3cd765f493e
suite <amiysd@163.com>
2018-10-07 22:52:20 +0800

    go utils

---
+++ log.go
@@ -0,0 +1,27 @@
+package goutils
+
+import (
+	"fmt"
+	"log"
+)
+
+/*
+  If there is an error print it, pass it
+*/
+func CheckAndPrintError(flag string, err error) {
+	if err != nil {
+		log.Println(flag+"\n", err)
+	}
+}
+
+func Print(arg ...interface{}) {
+	log.Print(arg)
+}
+
+func Println(arg ...interface{}) {
+	log.Println(arg)
+}
+
+func Stdout(arg ...interface{}) {
+	fmt.Println(arg...)
+}
