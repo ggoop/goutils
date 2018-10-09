9538fdd0fa9e7c24caac9572379a8668141cf500
suite <amiysd@163.com>
2018-10-09 20:35:38 +0800

    add gcast

---
+++ gcast/errors.go
@@ -0,0 +1,12 @@
+package gcast
+
+import (
+	"errors"
+)
+
+var (
+	errInvalidParams         = errors.New("Invalid params")
+	errUnsupportedType       = errors.New("Unsupported destination type")
+	errUnsupportedSourceType = errors.New("Unsupported source type")
+	ErrUnsettableValue       = errors.New("Can't set value")
+)
