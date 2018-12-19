d670e05bdc3ebfed97d0f70151ecd9851f96a5ad
吴运刚 <wuyg@yonyou.com>
2018-12-19 19:31:18 +0800

    add json

--- md/model.go
+++ md/model.go
@@ -5,13 +5,13 @@ import (
 )
 
 type Model struct {
-	ID        string     `gorm:"primary_key;size:100"`
-	CreatedAt time.Time  `gorm:"name:创建时间"`
-	UpdatedAt time.Time  `gorm:"name:更新时间"`
-	DeletedAt *time.Time `gorm:"name:删除时间" sql:"index"`
+	ID        string     `gorm:"primary_key;size:100" json:"id"`
+	CreatedAt time.Time  `gorm:"name:创建时间"  json:"created_at"`
+	UpdatedAt time.Time  `gorm:"name:更新时间" json:"updated_at"`
+	DeletedAt *time.Time `gorm:"name:删除时间" sql:"index" json:"deleted_at"`
 }
 type ModelUnscoped struct {
-	ID        string    `gorm:"primary_key;size:100"`
-	CreatedAt time.Time `gorm:"name:创建时间"`
-	UpdatedAt time.Time `gorm:"name:更新时间"`
+	ID        string    `gorm:"primary_key;size:100" json:"id"`
+	CreatedAt time.Time `gorm:"name:创建时间" json:"created_at"`
+	UpdatedAt time.Time `gorm:"name:更新时间" json:"updated_at"`
 }
