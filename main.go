0d39f42fcf80acfe62068db1d821468d82c12588
吴运刚 <wuyg@yonyou.com>
2019-01-07 18:04:12 +0800

    add guid

--- main.go
+++ main.go
@@ -1,32 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	"time"
-
 	"github.com/ggoop/goutils/glog"
 	"github.com/ggoop/goutils/utils"
 )
 
 func main() {
-	claims := make(map[string]interface{})
-	claims["aaa"] = 1111
-	claims["exp"] = time.Now().Add((-time.Hour) * time.Duration(1)).Unix()
-	claims["iat"] = time.Now().Unix()
-
-	str := utils.CreateJWTToken(claims, "aaa")
-
-	glog.Errorf("sss ,%v ", str)
-
-	mapClaims, err := utils.ParseJWTToken(str, "aaa")
-	licValues := make([]string, 0)
-	for k, v := range mapClaims {
-		licValues = append(licValues, fmt.Sprintf("%v:%v", k, v))
+	for i := 0; i < 100; i++ {
+		glog.Errorf("%v \r\n", utils.GUID())
 	}
-	if mapClaims.VerifyExpiresAt(time.Now().Unix(), false) {
-		glog.Errorf("sss ,%v ", str)
-	}
-	glog.Errorf("sss ,%v %v %v", mapClaims, err, strings.Join(licValues, ","))
-
 }
