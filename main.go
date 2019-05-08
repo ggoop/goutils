ebf01a488013b61130b5e5ba66fcd4e266f1836d
吴运刚 <wuyg@yonyou.com>
2019-05-08 14:36:36 +0800

    元数据调整

--- main.go
+++ main.go
@@ -11,39 +11,15 @@ func main() {
 	for i := 0; i < 100; i++ {
 		glog.Errorf("%v\r\n", utils.GUID())
 	}
-	// test_query()
+	test_query()
 }
 func test_query() {
 	mysql := repositories.NewMysqlRepo()
-	tx := mysql.Begin()
 
-	defer func() {
-		if r := recover(); r != nil {
-			tx.Rollback()
-		}
-	}()
+	exector:=query.NewExector("sys_oss_objects")
+	exector.Where("Oss.Ent.Name like 'a'")
+	q,_:=exector.PrepareQuery(mysql)
+	glog.Error(q.QueryExpr())
 
-	tx.AutoMigrate(query.Query{})
-
-	item := query.Query{}
-	item.ID = utils.GUID()
-	item.Name = "ddd"
-	if err := tx.Create(&item).Error; err != nil {
-		tx.Rollback()
-	}
-
-	item = query.Query{}
-	item.ID = "01e94b9a020ac7178f2bfa163e9054be"
-	item.Name = "ccc"
-	if err := tx.Create(&item).Error; err != nil {
-		tx.Rollback()
-	}
-
-	items := make([]query.Query, 0)
-	if err := mysql.Find(&items).Error; err != nil {
-		glog.Errorf("Find error is %s", err)
-	}
-	if err := tx.Commit().Error; err != nil {
-		glog.Errorf("Commit error is %s", err)
-	}
+	glog.Errorf("%v\r\n","dd")
 }
--- md/md.go
+++ md/md.go
@@ -47,13 +47,16 @@ func GetEntity(id string) *MDEntity {
 	}
 	item := &MDEntity{}
 	if err := di.Global.Invoke(func(db *repositories.MysqlRepo) {
-		db.Preload("Fields").Take(item, "id=? or code=?", id, id)
+		db.Preload("Fields").Take(item, "id=? or code=? or table_name=?", id, id,id)
 	}); err != nil {
 		glog.Errorf("di Provide error:%s", err)
 	}
 	if item.ID != "" {
 		mdCache[strings.ToLower(item.ID)] = item
 		mdCache[strings.ToLower(item.Code)] = item
+		if item.TableName!=""{
+			mdCache[strings.ToLower(item.TableName)] = item
+		}
 		return item
 	}
 	return nil
